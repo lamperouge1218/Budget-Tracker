141b40f34ce35a6fa4f8ff226a27931a0d4f5564
lamperouge <philbohn1791@gmail.com>
2021-11-03 15:03:23 -0400

    checkDatabase function complete

--- public/db.js
+++ public/db.js
@@ -22,3 +22,46 @@ request.onupgradeneeded = function (e) {
 request.onerror = function (e) {
   console.log(`Whoops! ${e.target.errorCode}`);
 };
+
+function checkDatabase() {
+  console.log("check db invoked");
+
+  // Open a transaction on your BudgetStore db
+  let transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  // Access the BudgetStore object
+  const store = transaction.objectStore("BudgetStore");
+
+  // Get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  // If the request was successful
+  getAll.onsuccess = function () {
+    // If there are items in the store, bulk add them when back online
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          // If the returned response is not empty
+          if (res.length !== 0) {
+            // Open another transaction to BudgetStore with the ability to read and write
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+
+            // Assign the current store to a variable
+            const currentStore = transaction.objectStore("BudgetStore");
+
+            // Clear existing entries because bulk add was successful
+            currentStore.clear();
+            console.log("Clearing Store");
+          }
+        });
+    }
+  };
+}
